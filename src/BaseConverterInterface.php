f41e86553d6fc94c5715cbf89795d654cba29473
Mustafa Talaeezadeh Khouzani <brother.t@live.com>
2020-03-10 17:08:50 +0330

    Refactoring to be more developer friendly. Removed QR-code functionalities.

--- src/BaseConverterInterface.php
+++ src/BaseConverterInterface.php
@@ -1,41 +1,41 @@
 <?php
-	
+
+/**
+ * interface.BaseConverterInterface
+ * Provides an interface for base conversion classes
+ *
+ * @author Mustafa Talaeezadeh Khouzani <brother.t@live.com>
+ * @version 5.8
+ * @copyright MIT
+ *
+ */
+
+namespace Khooz\Oath;
+
+
+interface BaseConverterInterface
+{
 	/**
-	 * interface.BaseConverterInterface
-	 * Provides an interface for base conversion classes
+	 * fromString
 	 *
-	 * @author Mustafa Talaeezadeh Khouzani <brother.t@live.com>
-	 * @version 5.8
-	 * @copyright MIT
+	 * Convert any string to a base32 string
+	 * This should be binary safe...
 	 *
+	 * @param string $str The string to convert
+	 *
+	 * @return string The converted base32 string
 	 */
+	public static function fromString (string $str) : string;
 
-	namespace Khooz\Oath;
-
-
-	interface BaseConverterInterface
-	{
-		/**
-		 * fromString
-		 *
-		 * Convert any string to a base32 string
-		 * This should be binary safe...
-		 *
-		 * @param string $str The string to convert
-		 *
-		 * @return string The converted base32 string
-		 */
-		public static function fromString (string $str) : string;
-
-		/**
-		 * toString
-		 *
-		 * Convert any base32 string to a normal string
-		 * This should be binary safe...
-		 *
-		 * @param string $str The base32 string to convert
-		 *
-		 * @return string The normal string
-		 */
-		public static function toString (string $str) : string;
-	}
+	/**
+	 * toString
+	 *
+	 * Convert any base32 string to a normal string
+	 * This should be binary safe...
+	 *
+	 * @param string $str The base32 string to convert
+	 *
+	 * @return string The normal string
+	 */
+	public static function toString (string $str) : string;
+}
